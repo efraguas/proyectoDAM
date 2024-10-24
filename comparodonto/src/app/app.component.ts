93ac977612ec8a3b35f5a92409bd0f013e46a7f8
Eric Fraguas <102832951+efraguas@users.noreply.github.com>
2024-10-24 17:17:12 +0200

    configuracion y diseño del navbar

--- comparodonto/src/app/app.component.ts
+++ comparodonto/src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {SidebarComponent} from './shared/sidebar/sidebar.component';
+import {NavbarComponent} from './shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'comparodonto';
 }
