6959e14fe85c1d10d5fcf52db63cfdafaac1211b
Eric Fraguas <102832951+efraguas@users.noreply.github.com>
2024-10-30 23:01:45 +0100

    Prueba de los endpoints, cambio en entity para manejar el campo id

--- odontoapi/src/main/java/com/comparodonto/odontoapi/odonto_api/Models/Productos.java
+++ odontoapi/src/main/java/com/comparodonto/odontoapi/odonto_api/Models/Productos.java
@@ -3,6 +3,7 @@ package com.comparodonto.odontoapi.odonto_api.Models;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.MongoId;
 
@@ -12,7 +13,7 @@ import org.springframework.data.mongodb.core.mapping.MongoId;
 @NoArgsConstructor  // Genera un constructor sin argumentos
 @AllArgsConstructor // Genera un constructor con todos los argumentos
 public class Productos {
-    @MongoId
+    @Id
     private String id;
     private String nombre;
     private String marca;
